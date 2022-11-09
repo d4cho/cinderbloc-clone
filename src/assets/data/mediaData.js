d185f1b723725973f09233d0f33d1c3d4c1fd350
bioDanCho <daniel.cho@thebioagency.com>
2022-11-09 18:20:39 -0500

    images animation and transition

---
+++ src/assets/data/mediaData.js
@@ -0,0 +1,45 @@
+import cinder1 from '../media/cinder1.jpeg';
+import cinder2 from '../media/cinder2.jpeg';
+import cinder3 from '../media/cinder3.jpeg';
+import cinder4 from '../media/cinder4.jpeg';
+import cinder5 from '../media/cinder5.jpeg';
+import cinder6 from '../media/cinder6.jpeg';
+import cinder7 from '../media/cinder7.mp4';
+import cinder8 from '../media/cinder8.jpeg';
+import cinder9 from '../media/cinder9.jpeg';
+import cinder10 from '../media/cinder10.jpeg';
+import cinder11 from '../media/cinder11.jpeg';
+import cinder12 from '../media/cinder12.png';
+import cinder13 from '../media/cinder13.jpeg';
+import cinder14 from '../media/cinder14.jpeg';
+import cinder15 from '../media/cinder15.jpeg';
+import cinder16 from '../media/cinder16.jpeg';
+import cinder17 from '../media/cinder17.jpeg';
+import cinder18 from '../media/cinder18.jpeg';
+import cinder19 from '../media/cinder19.jpeg';
+import cinder20 from '../media/cinder20.jpeg';
+import cinder21 from '../media/cinder21.jpeg';
+
+export const mediaData = [
+    { imgUrl: cinder1, orientation: 'portrait' },
+    { imgUrl: cinder2, orientation: 'landscape' },
+    { imgUrl: cinder3, orientation: 'landscape' },
+    { imgUrl: cinder4, orientation: 'landscape' },
+    { imgUrl: cinder5, orientation: 'landscape' },
+    { imgUrl: cinder6, orientation: 'portrait' },
+    { imgUrl: cinder7, orientation: 'landscape' },
+    { imgUrl: cinder8, orientation: 'portrait' },
+    { imgUrl: cinder9, orientation: 'landscape' },
+    { imgUrl: cinder10, orientation: 'landscape' },
+    { imgUrl: cinder11, orientation: 'portrait' },
+    { imgUrl: cinder12, orientation: 'landscape' },
+    { imgUrl: cinder13, orientation: 'portrait' },
+    { imgUrl: cinder14, orientation: 'landscape' },
+    { imgUrl: cinder15, orientation: 'landscape' },
+    { imgUrl: cinder16, orientation: 'landscape' },
+    { imgUrl: cinder17, orientation: 'landscape' },
+    { imgUrl: cinder18, orientation: 'portrait' },
+    { imgUrl: cinder19, orientation: 'landscape' },
+    { imgUrl: cinder20, orientation: 'landscape' },
+    { imgUrl: cinder21, orientation: 'landscape' },
+];
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,10 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getHeight, getWidth } from '../../utils/functions';
+import { mediaData } from '../../assets/data/mediaData';
 import './Home.scss';
 
 const Home = () => {
+    const [isAnimDone, setIsAnimDone] = useState(false);
+    const [dimensions, setDimensions] = useState({
+        width: getWidth(),
+        height: getHeight(),
+    });
+    const [imagePosition, setImagePosition] = useState({
+        x: 0,
+        y: 0,
+    });
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsAnimDone(true);
+        }, 4500);
+    }, []);
+
+    const handleMouseMove = (e) => {
+        let posX = e.clientX;
+        let posY = e.clientY;
+
+        if (
+            Math.abs(imagePosition.x - posX) > 80 ||
+            Math.abs(imagePosition.y - posY) > 80
+        ) {
+            setImagePosition({
+                x: posX,
+                y: posY,
+            });
+
+            setCurrentImageIndex((prevState) => {
+                return (prevState + 1) % mediaData.length;
+            });
+        }
+
+        setDimensions({
+            width: Math.abs(getWidth() - posX * 2), // x2 to account for left and right resizing
+            height: Math.abs(getHeight() - posY * 2), // x2 to account for top and bottom resizing
+        });
+    };
+
+    const renderSquare = (fromNumber) => {
+        let nextNumber = fromNumber - 1;
+        if (nextNumber > 0) {
+            return (
+                <div
+                    className={`Home_squares ${!isAnimDone && 'hidden'}`}
+                    style={{
+                        width: `${
+                            dimensions.width -
+                            dimensions.width * 0.05 * (20 - fromNumber)
+                        }px`,
+                        height: `${
+                            dimensions.height -
+                            dimensions.height * 0.05 * (20 - fromNumber)
+                        }px`,
+                        border: `${
+                            fromNumber === 20 ? 'none' : '1px solid #000'
+                        }`,
+                        '--opacity': `${1 - 0.01 * (20 - fromNumber)}`,
+                    }}
+                >
+                    {renderSquare(nextNumber)}
+                </div>
+            );
+        }
+    };
+
     return (
-        <div className='Home_wrapper'>
-            <div className='Home_ciderblocTextWrapper'>
+        <div className='Home_wrapper' onMouseMove={(e) => handleMouseMove(e)}>
+            <div
+                className={`Home_ciderblocTextWrapper ${isAnimDone && 'hide'}`}
+            >
                 <div className='block_section'>
                     <div className='block'>.</div>
                     <div className='text'>CINDER</div>
@@ -15,6 +87,46 @@ const Home = () => {
                     <div className='line'></div>
                 </div>
             </div>
+            {renderSquare(20)}
+
+            {/* {isAnimDone && imagePosition.x !== 0 && imagePosition.y !== 0 && (
+                <div
+                    className='Home_image'
+                    style={{
+                        top: `${imagePosition.y}px`,
+                        left: `${imagePosition.x}px`,
+                        '--imageUrl': `url(${mediaData[currentImageIndex]})`,
+                    }}
+                ></div>
+            )} */}
+
+            {isAnimDone &&
+                imagePosition.x !== 0 &&
+                imagePosition.y !== 0 &&
+                mediaData.map((media, idx) => {
+                    return (
+                        <div
+                            key={idx}
+                            className={`Home_image ${media.orientation} ${
+                                currentImageIndex === idx && 'showImage'
+                            }`}
+                            style={{
+                                top: `${imagePosition.y}px`,
+                                left: `${imagePosition.x}px`,
+                                '--imageUrl': `url(${media.imgUrl})`,
+                            }}
+                        >
+                            {idx === 6 && (
+                                <video loop muted autoPlay>
+                                    <source
+                                        src={media.imgUrl}
+                                        type='video/webm'
+                                    />
+                                </video>
+                            )}
+                        </div>
+                    );
+                })}
         </div>
     );
 };
