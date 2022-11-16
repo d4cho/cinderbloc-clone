9a221b23b3c68b305a6b0d449bb99296b4a2529c
bioDanCho <daniel.cho@thebioagency.com>
2022-11-16 16:47:57 -0500

    10 prince arthur media added

--- src/assets/data/GalleryData.js
+++ src/assets/data/GalleryData.js
@@ -18,6 +18,38 @@ import sher16 from '../media/sher-club-images/sher-16.jpeg';
 import sher17 from '../media/sher-club-images/sher-17.jpeg';
 import sher18 from '../media/sher-club-images/sher-18.jpeg';
 
+import prince0 from '../media/10-prince-arthur-images/prince0.mp4';
+import prince1 from '../media/10-prince-arthur-images/prince1.jpeg';
+import prince2 from '../media/10-prince-arthur-images/prince2.jpeg';
+import prince3 from '../media/10-prince-arthur-images/prince3.mp4';
+import prince4 from '../media/10-prince-arthur-images/prince4.jpeg';
+import prince5 from '../media/10-prince-arthur-images/prince5.jpeg';
+import prince6 from '../media/10-prince-arthur-images/prince6.jpeg';
+import prince7 from '../media/10-prince-arthur-images/prince7.jpeg';
+import prince8 from '../media/10-prince-arthur-images/prince8.jpeg';
+import prince9 from '../media/10-prince-arthur-images/prince9.jpeg';
+import prince10 from '../media/10-prince-arthur-images/prince10.jpeg';
+import prince11 from '../media/10-prince-arthur-images/prince11.jpeg';
+import prince12 from '../media/10-prince-arthur-images/prince12.jpeg';
+import prince13 from '../media/10-prince-arthur-images/prince13.jpeg';
+import prince14 from '../media/10-prince-arthur-images/prince14.mp4';
+import prince15 from '../media/10-prince-arthur-images/prince15.mp4';
+import prince16 from '../media/10-prince-arthur-images/prince16.jpeg';
+import prince17 from '../media/10-prince-arthur-images/prince17.jpeg';
+import prince18 from '../media/10-prince-arthur-images/prince18.jpeg';
+import prince19 from '../media/10-prince-arthur-images/prince19.jpeg';
+import prince20 from '../media/10-prince-arthur-images/prince20.jpeg';
+import prince21 from '../media/10-prince-arthur-images/prince21.jpeg';
+import prince22 from '../media/10-prince-arthur-images/prince22.jpeg';
+import prince23 from '../media/10-prince-arthur-images/prince23.jpeg';
+import prince24 from '../media/10-prince-arthur-images/prince24.jpeg';
+import prince25 from '../media/10-prince-arthur-images/prince25.jpeg';
+import prince26 from '../media/10-prince-arthur-images/prince26.jpeg';
+import prince27 from '../media/10-prince-arthur-images/prince27.jpeg';
+import prince28 from '../media/10-prince-arthur-images/prince28.jpeg';
+import prince29 from '../media/10-prince-arthur-images/prince29.jpeg';
+import prince30 from '../media/10-prince-arthur-images/prince30.jpeg';
+
 export const galleryData = {
     'sher-club': {
         title: 'sher club',
@@ -96,6 +128,135 @@ export const galleryData = {
             },
             {
                 url: sher18,
+                format: 'image',
+            },
+        ],
+    },
+    '10-prince-arthur': {
+        title: '10 prince arthur',
+        medias: [
+            {
+                url: prince0,
+                format: 'video',
+            },
+            {
+                url: prince1,
+                format: 'image',
+            },
+            {
+                url: prince2,
+                format: 'image',
+            },
+            {
+                url: prince3,
+                format: 'video',
+            },
+            {
+                url: prince4,
+                format: 'image',
+            },
+            {
+                url: prince5,
+                format: 'image',
+            },
+            {
+                url: prince6,
+                format: 'image',
+            },
+            {
+                url: prince7,
+                format: 'image',
+            },
+            {
+                url: prince8,
+                format: 'image',
+            },
+            {
+                url: prince9,
+                format: 'image',
+            },
+            {
+                url: prince10,
+                format: 'image',
+            },
+            {
+                url: prince11,
+                format: 'image',
+            },
+            {
+                url: prince12,
+                format: 'image',
+            },
+            {
+                url: prince13,
+                format: 'image',
+            },
+            {
+                url: prince14,
+                format: 'video',
+            },
+            {
+                url: prince15,
+                format: 'video',
+            },
+            {
+                url: prince16,
+                format: 'image',
+            },
+            {
+                url: prince17,
+                format: 'image',
+            },
+            {
+                url: prince18,
+                format: 'image',
+            },
+            {
+                url: prince19,
+                format: 'image',
+            },
+            {
+                url: prince20,
+                format: 'image',
+            },
+            {
+                url: prince21,
+                format: 'image',
+            },
+            {
+                url: prince22,
+                format: 'image',
+            },
+            {
+                url: prince23,
+                format: 'image',
+            },
+            {
+                url: prince24,
+                format: 'image',
+            },
+            {
+                url: prince25,
+                format: 'image',
+            },
+            {
+                url: prince26,
+                format: 'image',
+            },
+            {
+                url: prince27,
+                format: 'image',
+            },
+            {
+                url: prince28,
+                format: 'image',
+            },
+            {
+                url: prince29,
+                format: 'image',
+            },
+            {
+                url: prince30,
                 format: 'image',
             },
         ],
