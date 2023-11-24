ac361b84200e4c65e12ff8c5ba1c2f1f06fc1424
d4cho <danielcho91@gmail.com>
2023-11-24 09:17:43 -0500

    add bio widget

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import './App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Pages/Home/Home';
-import GalleryPage from './components/Pages/GalleryPage/GalleryPage';
-import AboutPage from './components/Pages/AboutPage/AboutPage';
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Pages/Home/Home";
+import GalleryPage from "./components/Pages/GalleryPage/GalleryPage";
+import AboutPage from "./components/Pages/AboutPage/AboutPage";
+import BioStorybookWidget from "bio-storybook-widget";
 
 function App() {
-    return (
-        <div className='App'>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<Home />}></Route>
-                    <Route
-                        exact
-                        path='/about---contact'
-                        element={<AboutPage />}
-                    ></Route>
-                    <Route
-                        path='/project/:galleryId'
-                        element={<GalleryPage />}
-                    ></Route>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <BioStorybookWidget projectSource={"cinderbloc"} />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route exact path='/about---contact' element={<AboutPage />}></Route>
+          <Route path='/project/:galleryId' element={<GalleryPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
